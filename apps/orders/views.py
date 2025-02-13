e6c52d9bfc8fcf2d39a73ce78e33f547f2d095cd
cristian@gaviria.org <Cristian Gaviria>
2025-02-13 16:10:17 -0500

    Implemented method views orders get_orders_woocommerce get_local_orders

--- apps/orders/views.py
+++ apps/orders/views.py
@@ -3,8 +3,11 @@ from rest_framework.decorators import api_view
 
 from utils.make_woocommerce_request import make_woocommerce_request
 
-from .models import OrderModel
+from .models import OrderModel, OrderItemModel
+from .serializers import OrderModelSerializer, OrderItemModelSerializer
+from apps.clients.models import ClientModel, AddressModel
 
+from apps.products.models import ProductModel
 @api_view(["GET"])
 def route_index(request):
     """Returns a list of all available routes.
@@ -31,22 +34,45 @@ def get_orders_woocommerce(request):
     Returns:
         Response: The response object.
     """
-    # TODO: Make a request to the WooCommerce API to get all orders and create them in the database.
-    # The creation of the orders should include OrderModel, OrderItemModel, ClientModel and AddressModel objects.
-    # The method make_woocommerce_request would be useful for making the request to the WooCommerce API.
-    # Use the endpoint 'orders' to get all orders from WooCommerce.
-    return Response({})
+    orders = make_woocommerce_request('orders', 'GET')
+    for obj_order in orders:
+
+        client = ClientModel.get_or_create_from_billing(obj_order['billing'])
+        print(client)
+        
+        address = AddressModel.get_or_create_from_shipping(obj_order['shipping'])
+        
+        order = OrderModel.objects.filter(external_id=obj_order['id']).first()
+        if not order:
+            order = OrderModel.objects.create(
+                client=client,
+                address=address,
+                total=obj_order['total'],
+                status=obj_order['status'],
+                external_id=obj_order['id'])
+        
+        for obj_item in obj_order['line_items']:
+            product = ProductModel.objects.filter(external_id=obj_item['id']).first()
+            if not product:
+                product = ProductModel.objects.create(
+                    name=obj_item['name'],
+                    description=obj_item['name'],
+                    price=obj_item['price'],
+                    stock=100,
+                    external_id=obj_item['product_id'])
+
+            order_item = OrderItemModel.objects.filter(order=order, product=product).first()
+            if not order_item:
+                order_item = OrderItemModel.objects.create(
+                quantity=obj_item['quantity'],
+                unit_price=obj_item['price'],
+                order=order,
+                product=product)
+
+    return Response(orders)
 
 @api_view(["GET"])
 def get_local_orders(request):
-    """Gets all orders from the database.
-
-    Args:
-        request (Request): The request object.
-
-    Returns:
-        Response: The response object.
-    """
-    # TODO: Create a serializer for the OrderModel and return the serialized data.
-    # The serializer should include the OrderModel, OrderItemModel, ClientModel and AddressModel objects.
-    return Response({})
+    orders = OrderModel.objects.all()
+    orders_serialized = OrderModelSerializer(orders, many=True)
+    return Response(orders_serialized.data)
