b6259c9e5b29b1330afafcfdb7b6e00f9bf4d3c7
cristian@gaviria.org <Cristian Gaviria>
2025-02-13 16:08:30 -0500

    Implemented ProductModel

--- apps/products/models.py
+++ apps/products/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # TODO: Implement the ProductModel
+
+class ProductModel(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    external_id = models.CharField(max_length=100, blank=True, null=True)
+    def __str__(self):
+        return self.name
