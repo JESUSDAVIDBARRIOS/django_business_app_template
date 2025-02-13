6ac9d8fb11a8aeffa90bda114ad3243da07ad2c8
cristian@gaviria.org <Cristian Gaviria>
2025-02-13 16:11:04 -0500

    Implemented method class get_or_create_from_billing ClientModel
    Implemented method class get_or_create_from_shipping AddressModel

--- apps/clients/models.py
+++ apps/clients/models.py
@@ -6,6 +6,15 @@ class ClientModel(models.Model):
     email = models.EmailField(max_length=100)
     phone = models.CharField(max_length=20)
 
+    @classmethod
+    def get_or_create_from_billing(cls, billing_info):
+        client, created = cls.objects.get_or_create(
+            name=billing_info['first_name']+' '+billing_info['last_name'],
+            email=billing_info['email'],
+            phone=billing_info['phone']
+        )
+        return client
+
     def __str__(self):
         return self.name
     
@@ -16,5 +25,14 @@ class AddressModel(models.Model):
     state = models.CharField(max_length=100)
     country = models.CharField(max_length=100)
 
+    @classmethod
+    def get_or_create_from_shipping(cls, shipping_info):
+        address, created = cls.objects.get_or_create(
+            address=shipping_info['address_1']+' '+shipping_info['address_2'],
+            city=shipping_info['city'],
+            state=shipping_info['state'],
+            country=shipping_info['country']
+        )
+        return address
     def __str__(self):
         return self.address
